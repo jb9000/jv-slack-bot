1dab9b6433ba5b069709a6fc92367922536b6aca
John McIntosh II <jmci10@hotmail.com>
2017-04-15 17:36:59 +0000

    add common conversions

---
+++ conversions.js
@@ -0,0 +1,34 @@
+module.exports = {
+  // LENGTH
+  "in2cm": "1in = 2.54cm",
+  "in2mm": "1in = 25.4mm",
+  "ft2m": "1ft = 0.3048m",
+  "ft2cm": "1ft = 30.48cm",
+  "ft2in": "1ft = 12in",
+  "mi2km": "1mi = 1.609344km",
+  "yd2m": "1yd = 0.9144m",
+  
+  // MASS
+  "lbs2kg": "1lb = 0.45359237kg",
+  "oz2g": "1oz = 28.349523125",
+  "oz2lbs": "1oz = 0.0625lbs",
+  
+  // VOLUME
+  "gal2l": "1gal = 3.785411784l",
+  "cup2mL": "1cup = 236.5882365 mL",
+  
+  // AREA
+  "ft^22ac": "1ft^2 = 2.2956841138659E-5 ac",
+  
+  // POWER
+  "kw2hp": "1kW = 1.3596216173039 horsepower (metric)",
+  
+  // ANGLE
+  "deg2rad": "1deg = 0.017453292519943 rad",
+  
+  // SPEED
+  "km/h2mi/h": "1km/h = 0.62137119223733 mi/h",
+  
+  // Temperature
+  "f2c": "1F = -17.222222222224C"
+};
---
+++ index.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parse");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( { extended: true } ));
+
+const server = app.listen(3000, () => {
+  console.log("Express server listening on port %d in %s mode",
+  server.address().port, app.settings.env);
+});
+
+/* *******************************
+/* HTTP Convert Me Slash Command
+/* ***************************** */
+
+app.post("/", (request, response) => {
+  convertImperialToMetric(request.body, response);
+});
+
+/*
+response:
+{ 
+  token: '2P429UX-------',
+  team_id: 'T1L---',
+  team_domain: 'ourtestaccount',
+  channel_id: 'C1L---',
+  channel_name: 'general',
+  user_id: 'U1L----',
+  user_name: 'ver2point0',
+  command: '/convertme',
+  text: 'p2k',
+  response_url: 'https://hooks.slack.com/commands/--- 
+}
+*/
+
+function convertImperialToMetric(query, response) {
+  
+}
