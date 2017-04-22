993ae88db0ccac855b6336556cf59a83194d403a
John McIntosh II <jmci10@hotmail.com>
2017-04-22 16:45:34 +0000

    update port number

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( { extended: true } ));
 
-const server = app.listen(3000, () => {
+const server = app.listen(8080, () => {
   console.log("Express server listening on port %d in %s mode",
   server.address().port, app.settings.env);
 });
